87a3aed774369b8a29dfb0a99926ab2369cdef77
YOSHI-DESKTOP\eevee <eeveelove0133@gmail.com>
2026-03-10 18:08:21 +0900

    ReadOnlyAttribute作成

---
+++ Assets/UnityGeneralTools/ReadOnlyAttribute/ReadOnlyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+
+namespace ReadOnlyAttribute
+{
+    [Conditional("UNITY_EDITOR")]
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+}
+
