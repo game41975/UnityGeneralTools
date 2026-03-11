af6b1af13cbe320b983fca5b5f69622b9e0d725a
YOSHI-DESKTOP\eevee <eeveelove0133@gmail.com>
2026-03-11 18:27:23 +0900

    ソースコード追加

---
+++ Assets/UnityGeneralTools/DataImporter/ScriptableObjectBase.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class ScriptableObjectBase : ScriptableObject
+{
+    //public abstract void InitParam<T>(T param) where T : ScriptableObjectParameterBase;
+    public virtual void InitParam(List<List<string>> list) 
+    {
+    }
+
+    public bool ConvertParam<T>(string key, System.Action<T> action)
+    {
+        bool convertSuccessed = false;
+        var type = typeof(T);
+        switch (System.Type.GetTypeCode(type))
+        {
+            case TypeCode.Int32:
+                {
+                    convertSuccessed = int.TryParse(key, out int result);
+                    if (convertSuccessed)
+                    {
+                        action.Invoke((T)(object)result);
+                    }
+                }
+                
+                break;
+            case TypeCode.Single:
+                {
+                    convertSuccessed = float.TryParse(key, out float result);
+                    if (convertSuccessed)
+                    {
+                        action.Invoke((T)(object)result);
+                    }
+                }
+                break;
+            case TypeCode.Double:
+                {
+                    convertSuccessed = double.TryParse(key, out double result);
+                    if (convertSuccessed)
+                    {
+                        action.Invoke((T)(object)result);
+                    }
+                }
+                break;
+            case TypeCode.String:
+                convertSuccessed = true;
+                action.Invoke((T)(object)key);
+                break;
+            default:
+                //対応外の型
+                break;
+        }
+
+        if (!convertSuccessed)
+        {
+            //TODO:変換失敗時のログ表示
+        }
+
+        return convertSuccessed;
+    }
+}
+
+public class ScriptableObjectParameterBase
+{
+}
---
+++ Assets/UnityGeneralTools/DataImporter/readme.txt
@@ -0,0 +1,1 @@
+Excelファイルの読み込みに「NPOI」を使用しているので、PluginsフォルダにNPOIのライブラリファイルを配置してください。https://www.nuget.org/packages/npoi/
